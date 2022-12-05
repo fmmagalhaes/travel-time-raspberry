b2b203184f4784a362550a8b441bb82fc3c353d8
fmmagalhaes <fran.mmagalhaes@gmail.com>
2022-12-05 22:39:47 +0000

    fix indentation

--- lcd_travel_time.py
+++ lcd_travel_time.py
@@ -18,29 +18,29 @@ def display_times():
 	print("----------------")
 
 def safe_display_times():
-    retries = 0
-    while retries < 10:
-        try:
-            display_times()
-            return
-        except (requests.ConnectionError, requests.Timeout):
-            print("Couldn't get response.")
-            print("----------------")
+	retries = 0
+	while retries < 10:
+		try:
+				display_times()
+				return
+		except (requests.ConnectionError, requests.Timeout):
+				print("Couldn't get response.")
+				print("----------------")
 
-        retries+=1
-        time.sleep(5)
+		retries+=1
+		time.sleep(5)
 
-    display.lcd_display_string("Failed to connect.", 1)
+	display.lcd_display_string("Failed to connect.", 1)
 
 try:
-    safe_display_times()
+	safe_display_times()
 
-    schedule.every(10).minutes.do(safe_display_times)
+	schedule.every(10).minutes.do(safe_display_times)
 
-    while True:
-        schedule.run_pending()
-        time.sleep(60)
+	while True:
+			schedule.run_pending()
+			time.sleep(60)
 
 except KeyboardInterrupt:
-    print("Cleaning up!")
-    display.lcd_clear()
+		print("Cleaning up!")
+		display.lcd_clear()
